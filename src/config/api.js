bb25707aad50580a299f48fd06556711a56de856
Kunj <kunjmpatel254@gmail.com>
2026-08-26 00:03:21 +0530

    Some changes in code

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,3 @@
 // Vite only exposes env vars prefixed with VITE_
-export const API_BASE_URL = import.meta.env.API_URL || "https://lockforgepwmserver.vercel.app/api/v1";
+export const API_BASE_URL = import.meta.env.VITE_API_URL;
 console.log('API_BASE_URL:', API_BASE_URL);
-// export const API_BASE_URL = import.meta.env.API_URL || 'http://localhost:5000/api/v1';
